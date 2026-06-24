0524e238ba1d39f81a70be53823341fdb5847ae1
XandMe Studio <kevindwinurhuda@gmail.com>
2026-06-24 11:52:00 +0700

    Add report module skeleton

---
+++ src/main/java/id/velioragardens/veliorasuite/module/report/ReportModule.java
@@ -0,0 +1,11 @@
+package id.velioragardens.veliorasuite.module.report;
+
+import id.velioragardens.veliorasuite.VelioraSuite;
+import id.velioragardens.veliorasuite.api.PlannedModule;
+
+public final class ReportModule extends PlannedModule {
+
+    public ReportModule(VelioraSuite plugin) {
+        super(plugin, "report", "VelioraReport");
+    }
+}
