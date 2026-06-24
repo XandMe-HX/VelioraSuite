88387382fc1ffe6fe0af226eec91981a16076c7c
XandMe Studio <kevindwinurhuda@gmail.com>
2026-06-24 11:52:33 +0700

    Add quest module skeleton

---
+++ src/main/java/id/velioragardens/veliorasuite/module/quest/QuestModule.java
@@ -0,0 +1,11 @@
+package id.velioragardens.veliorasuite.module.quest;
+
+import id.velioragardens.veliorasuite.VelioraSuite;
+import id.velioragardens.veliorasuite.api.PlannedModule;
+
+public final class QuestModule extends PlannedModule {
+
+    public QuestModule(VelioraSuite plugin) {
+        super(plugin, "quest", "VelioraQuest");
+    }
+}
