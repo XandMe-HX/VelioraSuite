2b9662bca2d541343f2b1c3fadfe229a2c3b110c
XandMe Studio <kevindwinurhuda@gmail.com>
2026-06-24 11:52:49 +0700

    Add boss module skeleton

---
+++ src/main/java/id/velioragardens/veliorasuite/module/boss/BossModule.java
@@ -0,0 +1,11 @@
+package id.velioragardens.veliorasuite.module.boss;
+
+import id.velioragardens.veliorasuite.VelioraSuite;
+import id.velioragardens.veliorasuite.api.PlannedModule;
+
+public final class BossModule extends PlannedModule {
+
+    public BossModule(VelioraSuite plugin) {
+        super(plugin, "boss", "VelioraBoss");
+    }
+}
