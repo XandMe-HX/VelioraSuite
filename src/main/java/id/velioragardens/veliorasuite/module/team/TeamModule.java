f31f4a516cf9c8511e7a1affe512276b663dfea6
XandMe Studio <kevindwinurhuda@gmail.com>
2026-06-24 11:51:43 +0700

    Add Team module

---
+++ src/main/java/id/velioragardens/veliorasuite/module/team/TeamModule.java
@@ -0,0 +1,10 @@
+package id.velioragardens.veliorasuite.module.team;
+
+import id.velioragardens.veliorasuite.VelioraSuite;
+import id.velioragardens.veliorasuite.api.PlannedModule;
+
+public final class TeamModule extends PlannedModule {
+    public TeamModule(VelioraSuite plugin) {
+        super(plugin, "team", "VelioraTeam");
+    }
+}
