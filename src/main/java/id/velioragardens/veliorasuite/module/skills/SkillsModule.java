80e0f6e113cb727e3b5d24643851d0617733c585
XandMe Studio <kevindwinurhuda@gmail.com>
2026-06-24 11:52:26 +0700

    Add skills module skeleton

---
+++ src/main/java/id/velioragardens/veliorasuite/module/skills/SkillsModule.java
@@ -0,0 +1,11 @@
+package id.velioragardens.veliorasuite.module.skills;
+
+import id.velioragardens.veliorasuite.VelioraSuite;
+import id.velioragardens.veliorasuite.api.PlannedModule;
+
+public final class SkillsModule extends PlannedModule {
+
+    public SkillsModule(VelioraSuite plugin) {
+        super(plugin, "skills", "VelioraSkills");
+    }
+}
