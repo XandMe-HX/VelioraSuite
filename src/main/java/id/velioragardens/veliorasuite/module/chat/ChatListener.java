363effdeda842a918ad03be59b7fb0c13bd5ebae
XandMe Studio <kevindwinurhuda@gmail.com>
2026-06-24 23:06:52 +0700

    Add VelioraChat listener

---
+++ src/main/java/id/velioragardens/veliorasuite/module/chat/ChatListener.java
@@ -0,0 +1,32 @@
+package id.velioragardens.veliorasuite.module.chat;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public final class ChatListener implements Listener {
+
+    private final ChatManager chatManager;
+
+    public ChatListener(ChatManager chatManager) {
+        this.chatManager = chatManager;
+    }
+
+    @EventHandler
+    public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
+        ChatManager.ChatProcessResult result = chatManager.processChat(event.getPlayer(), event.getMessage());
+
+        if (result.cancelled()) {
+            event.setCancelled(true);
+            return;
+        }
+
+        if (result.formatted()) {
+            event.setCancelled(true);
+            chatManager.broadcastFormatted(result.message());
+            return;
+        }
+
+        event.setMessage(result.message());
+    }
+}
