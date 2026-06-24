c92a5f54acf9d01d90b5ca15022468d8bcbf83e1
XandMe Studio <kevindwinurhuda@gmail.com>
2026-06-24 11:52:05 +0700

    Add fishing module skeleton

---
+++ src/main/java/id/velioragardens/veliorasuite/module/fishing/FishingModule.java
@@ -0,0 +1,11 @@
+package id.velioragardens.veliorasuite.module.fishing;
+
+import id.velioragardens.veliorasuite.VelioraSuite;
+import id.velioragardens.veliorasuite.api.PlannedModule;
+
+public final class FishingModule extends PlannedModule {
+
+    public FishingModule(VelioraSuite plugin) {
+        super(plugin, "fishing", "VelioraFishing");
+    }
+}
