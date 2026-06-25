dca74a9fbf5192a63549688a11f0178769a3a4ab
XandMe Studio <kevindwinurhuda@gmail.com>
2026-06-25 18:42:43 +0700

    Remove unused lag module registration

--- src/main/java/id/velioragardens/veliorasuite/VelioraSuite.java
+++ src/main/java/id/velioragardens/veliorasuite/VelioraSuite.java
@@ -8,7 +8,6 @@ import id.velioragardens.veliorasuite.core.ModuleManager;
 import id.velioragardens.veliorasuite.module.announcement.AnnouncementModule;
 import id.velioragardens.veliorasuite.module.boss.BossModule;
 import id.velioragardens.veliorasuite.module.chat.ChatModule;
-import id.velioragardens.veliorasuite.module.clearlag.ClearLagModule;
 import id.velioragardens.veliorasuite.module.fishing.FishingModule;
 import id.velioragardens.veliorasuite.module.guide.GuideModule;
 import id.velioragardens.veliorasuite.module.kits.KitsModule;
@@ -83,7 +82,6 @@ public final class VelioraSuite extends JavaPlugin {
         moduleManager.register(new KitsModule(this));
         moduleManager.register(new ReportModule(this));
         moduleManager.register(new ChatModule(this));
-        moduleManager.register(new ClearLagModule(this));
         moduleManager.register(new FishingModule(this));
         moduleManager.register(new SkillsModule(this));
         moduleManager.register(new QuestModule(this));
