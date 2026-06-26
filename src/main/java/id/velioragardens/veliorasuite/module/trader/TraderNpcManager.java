52f4a8d1908da36105f3ec8fb386340caeddda04
XandMe Studio <kevindwinurhuda@gmail.com>
2026-06-27 01:51:44 +0700

    Improve VelioraTrader NPC visibility

--- src/main/java/id/velioragardens/veliorasuite/module/trader/TraderNpcManager.java
+++ src/main/java/id/velioragardens/veliorasuite/module/trader/TraderNpcManager.java
@@ -3,6 +3,7 @@ package id.velioragardens.veliorasuite.module.trader;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -25,24 +26,40 @@ public final class TraderNpcManager implements Listener {
         this.traderManager = traderManager;
     }
 
-    public void spawn(Location location) {
-        if (location == null || location.getWorld() == null) return;
+    public boolean spawn(Location origin) {
+        if (origin == null || origin.getWorld() == null) return false;
         remove();
-        Location npcLocation = location.clone().add(0.5D, 1.0D, 0.5D);
-        traderEntity = location.getWorld().spawnEntity(npcLocation, configManager.getNpcType());
-        if (traderEntity instanceof LivingEntity living) freeze(living, configManager.isNpcGravity());
+        Location npcLocation = origin.clone().add(configManager.getNpcOffsetX(), configManager.getNpcOffsetY(), configManager.getNpcOffsetZ());
+        npcLocation.getChunk().load(true);
+        traderEntity = origin.getWorld().spawnEntity(npcLocation, configManager.getNpcType());
+        traderEntity.teleport(npcLocation);
+        traderEntity.addScoreboardTag("velioratrader_npc");
         traderEntity.setCustomName(configManager.color(configManager.getNpcName()));
         traderEntity.setCustomNameVisible(true);
+        traderEntity.setGlowing(configManager.isNpcGlowing());
+        traderEntity.setPersistent(true);
+        if (traderEntity instanceof LivingEntity living) freeze(living, configManager.isNpcGravity(), true);
+        if (traderEntity instanceof Villager villager) configureVillager(villager);
         traderEntities.add(traderEntity.getUniqueId());
 
         if (configManager.isCompanionEnabled()) {
-            Location companionLocation = npcLocation.clone().add(1.5D, 0.0D, 0.0D);
-            companionEntity = location.getWorld().spawnEntity(companionLocation, configManager.getCompanionType());
-            if (companionEntity instanceof LivingEntity living) freeze(living, true);
-            companionEntity.setCustomName(configManager.color("&eTrader Companion"));
-            companionEntity.setCustomNameVisible(false);
+            Location companionLocation = origin.clone().add(configManager.getCompanionOffsetX(), configManager.getCompanionOffsetY(), configManager.getCompanionOffsetZ());
+            companionLocation.getChunk().load(true);
+            companionEntity = origin.getWorld().spawnEntity(companionLocation, configManager.getCompanionType());
+            companionEntity.teleport(companionLocation);
+            companionEntity.addScoreboardTag("velioratrader_companion");
+            companionEntity.setCustomName(configManager.color(configManager.getCompanionName()));
+            companionEntity.setCustomNameVisible(configManager.isCompanionNameVisible());
+            companionEntity.setPersistent(true);
+            if (companionEntity instanceof LivingEntity living) freeze(living, true, false);
             traderEntities.add(companionEntity.getUniqueId());
         }
+
+        if (configManager.isDebugSpawn()) {
+            org.bukkit.Bukkit.getLogger().info("VelioraTrader debug: NPC " + configManager.getNpcType() + " at " + format(npcLocation) + " uuid=" + traderEntity.getUniqueId());
+            if (companionEntity != null) org.bukkit.Bukkit.getLogger().info("VelioraTrader debug: companion " + configManager.getCompanionType() + " at " + format(companionEntity.getLocation()) + " uuid=" + companionEntity.getUniqueId());
+        }
+        return true;
     }
 
     public void remove() {
@@ -70,12 +87,23 @@ public final class TraderNpcManager implements Listener {
         event.setCancelled(true);
     }
 
-    private void freeze(LivingEntity entity, boolean gravity) {
+    private void freeze(LivingEntity entity, boolean gravity, boolean useNpcSilent) {
         entity.setAI(false);
         entity.setInvulnerable(true);
-        entity.setSilent(configManager.isNpcSilent());
+        entity.setSilent(useNpcSilent ? configManager.isNpcSilent() : true);
         entity.setGravity(gravity);
         entity.setCollidable(false);
         entity.setRemoveWhenFarAway(false);
+        entity.setPersistent(true);
+    }
+
+    private void configureVillager(Villager villager) {
+        villager.setAdult();
+        try { villager.setProfession(Villager.Profession.CARTOGRAPHER); } catch (Exception ignored) { }
+        try { villager.setVillagerType(Villager.Type.PLAINS); } catch (Exception ignored) { }
+    }
+
+    private String format(Location location) {
+        return (location.getWorld() == null ? "world" : location.getWorld().getName()) + " " + location.getBlockX() + " " + location.getBlockY() + " " + location.getBlockZ();
     }
 }
